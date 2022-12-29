fcb66a646bcfdbdf684257381c814f0b1dbccac0
realyoungwoody <alexryabkov5559@gmail.com>
2022-12-29 14:06:37 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,137 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Чтобы начать, нажмите ENTER или RETURN");
+        String ENTER = s.nextLine();
+        System.out.println("Сколько будет 35+97? ");
+        int i = s.nextInt();
+        if (i == 132)
+            System.out.println("Так держать!");
+
+        else {
+            if (i!= 132)
+                System.out.println("ОШИБКА!!!!!!!!!");
+            System.out.println("Что-то ты тупанула, попробуй ещё разок: ");
+            int b = s.nextInt();
+            if (b == 132)
+                System.out.println("Ну наконец-то!");
+            else {
+                System.out.println("Да что ж такое-то?");
+                System.out.println("Давай ещё раз!");
+                int a = s.nextInt();
+                if (a == 132)
+                    System.out.println("Неужели!");
+                else {
+                    System.out.println("Видимо сегодня не твой день!");
+                }
+            }
+        }
+        System.out.println("Погнали дальше!!!");
+        System.out.println("Сколько будет 58+79?");
+        int z = s.nextInt();
+        if (z == 137)
+            System.out.println("Так держать!");
+
+        else {
+            if (z!= 137)
+                System.out.println("ОШИБКА!!!!!!!!!");
+            System.out.println("Что-то ты тупанула, попробуй ещё разок: ");
+            int d = s.nextInt();
+            if (d == 137)
+                System.out.println("Ну наконец-то!");
+            else {
+                System.out.println("Да что ж такое-то?");
+                System.out.println("Давай ещё раз!");
+                int v = s.nextInt();
+                if (v == 137)
+                    System.out.println("Неужели!");
+                else {
+                    System.out.println("Видимо сегодня не твой день!");
+                }
+            }
+        }
+        System.out.println("Погнали дальше!!!");
+        System.out.println("Сколько будет 153-84?");
+        int n = s.nextInt();
+        if (n == 69)
+            System.out.println("Так держать!");
+        else {
+            if (i != 69)
+                System.out.println("ОШИБКА!!!!!!!!!");
+            System.out.println("Что-то ты тупанула, попробуй ещё разок: ");
+            int h = s.nextInt();
+            if (h == 69)
+                System.out.println("Ну наконец-то!");
+            else {
+                System.out.println("Да что ж такое-то?");
+                System.out.println("Давай ещё раз!");
+                int v = s.nextInt();
+                if (v == 69)
+                    System.out.println("Неужели!");
+                else {
+                    System.out.println("Видимо сегодня не твой день!");
+                }
+            }
+
+            }
+            System.out.println("Погнали дальше!!!");
+            System.out.println("Сколько будет 24*8?");
+            int q = s.nextInt();
+            if (q == 192)
+                System.out.println("Так держать!");
+
+            else {
+                if (q != 192)
+                    System.out.println("ОШИБКА!!!!!!!!!");
+                System.out.println("Что-то ты тупанула, попробуй ещё разок: ");
+                int g = s.nextInt();
+                if (g == 192)
+                    System.out.println("Ну наконец-то!");
+                else {
+                    System.out.println("Да что ж такое-то?");
+                    System.out.println("Давай ещё раз!");
+                    int f = s.nextInt();
+                    if (f == 192)
+                        System.out.println("Неужели!");
+                    else {
+                        System.out.println("Видимо сегодня не твой день!");
+                    }
+                }
+            }
+            System.out.println("Погнали дальше!!!");
+                    System.out.println("Сколько будет 26:2?");
+                    int t = s.nextInt();
+                    if (t == 13)
+                        System.out.println("Так держать!");
+
+                    else {
+                        if (t!= 13)
+                            System.out.println("ОШИБКА!!!!!!!!!");
+                        System.out.println("Что-то ты тупанула, попробуй ещё разок: ");
+                        int y = s.nextInt();
+                        if (y== 13)
+                            System.out.println("Ну наконец-то!");
+                        else {
+                            System.out.println("Да что ж такое-то?");
+                            System.out.println("Давай ещё раз!");
+                            int o = s.nextInt();
+                            if (o == 13)
+                                System.out.println("Неужели!");
+                            else {
+                                System.out.println("Видимо сегодня не твой день!");
+        }
+                            s.close();
+
+    }
+
+            {
+
+            }
+        }
+        {
+        }
+
+
+    }}
